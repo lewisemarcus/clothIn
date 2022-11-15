97438d800c15a6a5851409995554ad163b48e2f7
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-11-14 23:13:17 -0800

    navbar updates

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,22 @@ import { ThemeProvider } from "@mui/material/styles"
 import { CssBaseline } from "@mui/material"
 import { themes } from "./theme"
 
+import { Provider } from "react-redux"
+import { configureStore } from "@reduxjs/toolkit"
+import cartReducer from "./state"
+
+const store = configureStore({
+    reducer: { cart: cartReducer },
+})
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={themes}>
-            <CssBaseline />
-            <App />
-        </ThemeProvider>
+        <Provider store={store}>
+            <ThemeProvider theme={themes}>
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
+        </Provider>
     </React.StrictMode>,
 )
