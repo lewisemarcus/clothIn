3c552778e3cf1e28f018f67236867c863014fb3f
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-11-14 19:59:43 -0800

    redux and action setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,12 @@ import "./index.css"
 import App from "./App"
 import { ThemeProvider } from "@mui/material/styles"
 import { CssBaseline } from "@mui/material"
-import { theme } from "./theme"
+import { themes } from "./theme"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={themes}>
             <CssBaseline />
             <App />
         </ThemeProvider>
