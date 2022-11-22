822f63ea143d6b61d009be13c7ad84fb63b3fb15
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-11-21 22:51:45 -0800

    carousel arrows

---
+++ client/src/scenes/home/MainCarousel.jsx
@@ -0,0 +1,60 @@
+import { Box, Typography, IconButton, useMediaQuery } from "@mui/material"
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore"
+import NavigateNextIcon from "@mui/icons-material/NavigateNext"
+import { shades } from "../../theme"
+
+//imports all images from assets folder
+const importAll = (r) =>
+    r.keys.reduce((acc, item) => {
+        acc[item.replace("./", "")] = r(item)
+        return acc
+    }, {})
+export const heroTextureImports = importAll(
+    require.context("../../assets", false, /\.(png|jpe?g|svg)$/),
+)
+const MainCarousel = () => {
+    const isNonMobile = useMediaQuery("min-width:600px")
+
+    return (
+        <Carousel
+            infiniteLoop={true}
+            showIndicators={false}
+            showStatus={false}
+            showThumbs={false}
+            renderArrowPrev={(onClickHandler, hasPrev, label) => (
+                <IconButton
+                    onClick={onClickHandler}
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "0",
+                        color: "white",
+                        padding: "5px",
+                        zIndex: "10",
+                    }}
+                >
+                    <NavigateBeforeIcon sx={{ fontSize: 40 }} />
+                </IconButton>
+            )}
+            renderArrowNext={(onClickHandler, hasNext, label) => (
+                <IconButton
+                    onClick={onClickHandler}
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        right: "0",
+                        color: "white",
+                        padding: "5px",
+                        zIndex: "10",
+                    }}
+                >
+                    <NavigateNextIcon sx={{ fontSize: 40 }} />
+                </IconButton>
+            )}
+        />
+    )
+}
+
+export default MainCarousel
