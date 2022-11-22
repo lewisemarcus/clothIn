95125fd15ee69313f61cf11d604729fd355ae7a9
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-11-21 22:10:46 -0800

    item component

--- client/src/components/item.jsx
+++ client/src/components/item.jsx
@@ -72,9 +72,31 @@ const Item = ({ item, width }) => {
                             </IconButton>
                         </Box>
                         {/* BUTTON */}
-                        <Button>Add To Cart</Button>
+                        <Button
+                            onClick={() =>
+                                dispatch(
+                                    addToCart({ item: { ...item, count } }),
+                                )
+                            }
+                            sx={{
+                                backgroundColor: shades.primary[300],
+                                color: "white",
+                            }}
+                        >
+                            Add To Cart
+                        </Button>
                     </Box>
                 </Box>
+            </Box>
+
+            <Box mt="3px">
+                <Typography variant="subtitle2" color={neutral.dark}>
+                    {category
+                        .replace(/[A-Z]/g, " $1")
+                        .replace(/^./, (str) => str.toUpperCase())}
+                </Typography>
+                <Typography>{name}</Typography>
+                <Typography fontWeight="bold">{price}</Typography>
             </Box>
         </Box>
     )
