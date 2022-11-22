5d95a6e347bc6e9b8724af0f2ebc23d317629150
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-11-22 00:17:42 -0800

    more home page carousel edits

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -53,7 +53,47 @@ const MainCarousel = () => {
                     <NavigateNextIcon sx={{ fontSize: 40 }} />
                 </IconButton>
             )}
-        />
+        >
+            {Object.values(heroTextureImports).map((texture, index) => (
+                <Box key={`carousel-image-${index}`}>
+                    <img
+                        src={texture}
+                        alt={`carousel-${index}`}
+                        style={{
+                            width: "100%",
+                            height: "700px",
+                            objectFit: "cover",
+                            backgroundAttachment: "fixed",
+                        }}
+                    />
+                    <Box
+                        color="white"
+                        padding="20px"
+                        borderRadius="1px"
+                        textAlign="left"
+                        backgroundColor="rgba(0, 0, 0, 0.4)"
+                        position="absolute"
+                        top="46%"
+                        left={isNonMobile ? "10%" : "0"}
+                        right={isNonMobile ? undefined : "0"}
+                        margin={isNonMobile ? undefined : "0 auto"}
+                        maxWidth={isNonMobile ? undefined : "240px"}
+                    >
+                        <Typography color={shades.secondary[200]}>
+                            -- NEW ITEMS
+                        </Typography>
+                        <Typography variant="h1">Winter Sale</Typography>
+                        <Typography
+                            fontWeight="bold"
+                            color={shades.secondary[300]}
+                            sx={{}}
+                        >
+                            Winter Sale
+                        </Typography>
+                    </Box>
+                </Box>
+            ))}
+        </Carousel>
     )
 }
 
