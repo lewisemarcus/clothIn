c55240fadbe62d5c9ef23228a5bd2fc6e07876d0
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-12-12 14:01:43 -0800

    item details page

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -1,5 +1,69 @@
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { IconButton, Box, Typography, Button, Tabs, Tab } from "@mui/material"
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined"
+import AddIcon from "@mui/icons-material/Add"
+import RemoveIcon from "@mui/icons-material/Remove"
+import { shades } from "../../theme"
+import { addToCart } from "../../state"
+import { useParams } from "react-router-dom"
+import Item from "../../components/Item"
+
 const ItemDetails = () => {
-    return <div>itemDetails</div>
+    const dispatch = useDispatch()
+    const { itemId } = useParams()
+
+    const [value, setValue] = useState("description")
+    const [count, setCount] = useState(1)
+    const [item, setItem] = useState(null)
+    const [items, setItems] = useState([])
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue)
+    }
+
+    const getItem = async () => {
+        const item = await fetch(
+            `http://localhost:1337/api/items/${itemId}?populate=image`,
+            {
+                method: "GET",
+            },
+        )
+        const itemJson = await item.json()
+        setItem(itemJson.data)
+    }
+    const getItems = async () => {
+        const items = await fetch(
+            "http://localhost:1337/api/items?populate=image",
+            { method: "GET" },
+        )
+
+        const itemsJson = await items.json()
+        setItems(itemsJson.data)
+    }
+
+    useEffect(() => {
+        getItem()
+        getItems()
+    }, [itemId]) //eslint-disable-line react-hooks/exhaustive-deps
+    return (
+        <Box width="80%" m="80px auto">
+            <Box display="flex" flexWrap="wrap" columnGap="40px">
+                {/* IMAGES */}
+                <Box flex="1 1 40%" mb="40px">
+                    <img
+                        alt={item?.name}
+                        width="100%"
+                        height="100%"
+                        src={`http://localhost:1337/${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                        style={{ objectFit: "contain" }}
+                    />
+                </Box>
+                {/* ACTIONS */}
+                <Box flex="1 1 50%"></Box>
+            </Box>
+        </Box>
+    )
 }
 
 export default ItemDetails
