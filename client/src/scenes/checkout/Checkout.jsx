272a667363b19a731fbbfa9c90b348a891d3c641
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-12-15 11:47:36 -0800

    checkout validation schema

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -45,14 +45,35 @@ const checkoutSchema = [
         }),
         shippingAddress: yup.object().shape({
             isSameAddress: yup.boolean(),
-            firstName: yup.string().required("required"),
-            lastName: yup.string().required("required"),
-            country: yup.string().required("required"),
-            street1: yup.string().required("required"),
+            firstName: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+            lastName: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+            country: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+            street1: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
             street2: yup.string(),
-            city: yup.string().required("required"),
-            state: yup.string().required("required"),
-            zipCode: yup.string().required("required"),
+            city: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+            state: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
+            zipCode: yup.string().when("isSameAddress", {
+                is: false,
+                then: yup.string().required("required"),
+            }),
         }),
     }),
 ]
