96c46477df6b861588bb92aea8c216e0a53d8f2c
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-12-19 15:43:45 -0800

    shipping page setup

--- client/src/scenes/checkout/Shipping.jsx
+++ client/src/scenes/checkout/Shipping.jsx
@@ -12,17 +12,52 @@ const Shipping = (
     return (
         <Box m="30px auto">
             {/* BILLING FORM */}
-            <Typography sx={{ mb: "15px" }} fontSize="18px">
-                Billing Information
-            </Typography>
-            <AddressForm
-                type="billingAddress"
-                value={values.billingAddress}
-                errors={errors}
-                touched={touched}
-                handleChange={handleChange}
-                handleBlur={handleBlur}
-            />
+            <Box>
+                <Typography sx={{ mb: "15px" }} fontSize="18px">
+                    Billing Information
+                </Typography>
+                <AddressForm
+                    type="billingAddress"
+                    value={values.billingAddress}
+                    errors={errors}
+                    touched={touched}
+                    handleChange={handleChange}
+                    handleBlur={handleBlur}
+                />
+            </Box>
+            <Box mb="20px">
+                <FormControlLabel
+                    label="Same for Shipping Address"
+                    control={
+                        <Checkbox
+                            defaultChecked
+                            value={values.shippingAddress.isSameAddress}
+                            onChange={() => {
+                                setFieldValue(
+                                    "shippingAddress.isSameAddress",
+                                    !values.shippingAddress.isSameAddress,
+                                )
+                            }}
+                        />
+                    }
+                />
+            </Box>
+            {/* Shipping Form */}
+            {!values.shippingAddress.isSameAddress && (
+                <Box>
+                    <Typography sx={{ mb: "15px" }} fontSize="18px">
+                        Shipping Information
+                    </Typography>
+                    <AddressForm
+                        type="shippingAddress"
+                        value={values.shippingAddress}
+                        errors={errors}
+                        touched={touched}
+                        handleChange={handleChange}
+                        handleBlur={handleBlur}
+                    />
+                </Box>
+            )}
         </Box>
     )
 }
